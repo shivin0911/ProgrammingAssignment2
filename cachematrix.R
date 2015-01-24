c4a13ecd7fd2c916e309dba7ce969cd5ffdd2520
Shivin Singh <shivin0911@gmail.com>
2015-01-25 03:31:28 +0530

    last
    done

--- cachematrix.R
+++ cachematrix.R
@@ -1,61 +1,41 @@
-<<<<<<< HEAD
-## SHIVIN SINGH 
-=======
-## shivin singh
->>>>>>> origin/master
-## 25-01-2015
-## makeCacheMatrix 
-## 1. Initializes a variable 'inv' 
-##    (which will be used to save inverse matrix latter, i.e. a cached data);
-## 2. Provides function get() to obtain "raw" matrix (of which one needs to find 
-##    its inverse);
+  ## SHIVIN SINGH 
+  ## 25-01-2015
+  ## makeCacheMatrix 
+  ## 1. Initializes a variable 'inv' 
+  ##    (which will be used to save inverse matrix latter, i.e. a cached data);
+  ## 2. Provides function get() to obtain "raw" matrix (of which one needs to find 
+  ##    its inverse);
 ## 3. Provides function setinv() to assign computed inverse matrix (of x) to m;
 ## 4. Provides function getinv() to obtain the cached inverse matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
- inv <- NULL
- set <- function(y) {
-   x <<- y
-   inv <<- NULL
- }
- get <- function(){ x}
- setinv <- function(inverse){ inv <<- inverse}
- getinv <- function() {inv}
- list(set = set, get = get,
-      setinv = setinv,
-      getinv = getinv)
+  
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function(){ x}
+  setinv <- function(inverse){ inv <<- inverse}
+  getinv <- function() {inv}
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
-
-
-<<<<<<< HEAD
 
 ## calculating the inverse of the matrix
 ##also checks whether the inverse has already been calculated
-=======
->>>>>>> origin/master
 
-
-## checks whether the inverse has already been calculated
-## calculating the inverse of the matrix
 cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
   inv <- x$getinv()
-<<<<<<< HEAD
   if(!is.null(inv)) {                # cached inverse found
-=======
-  if(!is.null(inv)) {  #cached inverse found
->>>>>>> origin/master
     message("getting cached data")
     return(inv)
   }
   message("calculating for the first time")
   data <- x$get()
-<<<<<<< HEAD
   inv <- solve(data, ...)           #  finds inverse of the matrix
   x$setinv(inv)
   inv                                #  returning the obtained matrix
-=======
-  inv <- solve(data, ...)#finds inverse of the matrix
-  x$setinv(inv)
-  inv#returning the obtained matrix
->>>>>>> origin/master
 }
